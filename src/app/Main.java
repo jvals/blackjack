4f77d7751e6a8574c41118b5ec98b8e185426710
jvals <jvals@users.noreply.github.com>
2017-09-20 11:22:24 +0200

    Add top-down view of program in main

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,10 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-
+        // Read from commandline args
+        // If file is specified, read file and convert the contents to an array of cards
+        // Else, generate a new shuffled deck of cards
+        // Create a new game, with the array of cards as input
+        // Run the game
     }
 }
