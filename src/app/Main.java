03136b8d42963289ef7839dd9520dfaa88858146
jvals <jvals@users.noreply.github.com>
2017-09-20 11:39:56 +0200

    Set up the cases for the initial setup

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,8 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         // Read from commandline args
-        // If file is specified, read file and convert the contents to an array of cards
-        // Else, generate a new shuffled deck of cards
+        switch (args.length) {
+            case 0:
+                // Create new shuffled deck of cards
+                break;
+            case 1:
+                // Read from specified file
+                break;
+            default:
+                // Print usage
+                System.out.println("This program takes exactly 0 or 1 command line arguments.\n" +
+                        "No argument means 'Create a new shuffled deck of cards'.\n" +
+                        "One argument means 'Read a deck of cards from the specified file'."
+                );
+                System.exit(1);
+                break;
+        }
         // Create a new game, with the array of cards as input
         // Run the game
     }
