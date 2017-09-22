775b94e59482212d44889fb3eb1277dbfe8de0be
jvals <jvals@users.noreply.github.com>
2017-09-22 23:21:14 +0200

    Add game class

---
+++ src/app/Game.java
@@ -0,0 +1,123 @@
+package app;
+
+import java.util.ArrayList;
+
+public class Game {
+    private Deck deck;
+    private Player winner = null;
+    private Player player;
+    private Player dealer;
+
+    public Game(Deck deck, Player player, Player dealer) {
+        this.deck = deck;
+        this.player = player;
+        this.dealer = dealer;
+    }
+
+    /**
+     * @return The winner of the game
+     */
+    public Player getWinner() {
+        if (winner == null) {
+            throw new RuntimeException("Winner is yet to be determined");
+        }
+
+        return winner;
+    }
+
+    public void run() {
+        // All players draw two cards in the beginning
+        drawCard(player);
+        drawCard(player);
+
+        // Dealer also draws two cards
+        drawCard(dealer);
+        drawCard(dealer);
+
+        // If the player draws 21, they win
+        if (isTwentyOne(player)) {
+            winner = player;
+            return;
+        }
+
+        // If the both the dealer and the player bust, the dealer wins
+        if (isBust(dealer) && isBust(player)) {
+            winner = dealer;
+            return;
+        }
+
+        // If only the dealer busts, the player wins
+        if (isBust(dealer)) {
+            winner = player;
+            return;
+        }
+
+        // If only the player busts, the dealer wins
+        if (isBust(player)) {
+            winner = dealer;
+            return;
+        }
+
+        // Player draw cards until their score is greater than or equal to 17
+        while(player.getScore() < 17) {
+            drawCard(player);
+        }
+
+        if (isBust(player)) {
+            winner = dealer;
+            return;
+        }
+
+        if (isTwentyOne(player)) {
+            winner = player;
+            return;
+        }
+
+        // The dealer draws until their hand is better than the player hand
+        while(dealer.getScore() <= player.getScore()) {
+            drawCard(dealer);
+        }
+
+        if (isBust(dealer)) {
+            winner = player;
+            return;
+        }
+
+        // At this point, the dealer must have a better hand than the player
+        winner = dealer;
+    }
+
+    public void drawCard(Player player) {
+        Card card = deck.drawCard();
+        player.addCard(card);
+
+        if (card.getFace() == 1) {
+            // Ace has the special value of 11 in our game
+            player.increaseScore(11);
+        } else {
+            // Picture cards add 10 to score
+            player.increaseScore(Integer.max(card.getFace(), 10));
+        }
+    }
+
+    public boolean isTwentyOne(Player player) {
+        return (player.getScore() == 21);
+    }
+
+    public boolean isBust(Player player) {
+        return player.getScore() > 21;
+    }
+
+    public void printResults() {
+        // Print the name of the winner
+        System.out.println(this.winner.getName());
+
+        // Print the players hand
+        System.out.print(player.getName() + ": ");
+        player.printHand();
+
+        // Print the dealers hand
+        System.out.print(dealer.getName() + ": ");
+        dealer.printHand();
+    }
+}
